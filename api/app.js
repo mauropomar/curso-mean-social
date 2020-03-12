509aef86c59003e082effa85640ad67e14cf444a
Tony <antonio.navarro@gmail.com>
2020-03-12 08:46:05 -0400

    Configuracion de messages

--- api/app.js
+++ api/app.js
@@ -12,8 +12,16 @@ var messages_routes = require('./routes/message');
 
 //middleware
 
-app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
+//cors
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+});
 
 //rutas
 app.use('/api', user_routes);
